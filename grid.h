23be43cbc3d9e06e1f7b3743020c4b5288db8b05
Shebboliciouss <a1899570@adelaide.edu.au>
2023-10-05 22:30:29 +0800

    changes

---
+++ grid.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#ifndef GRID_h
+#define GRID_h
+
+class grid {
+
+    protected:
+//initialise weight and length of grid
+    int length;
+
+    int width;
+
+   public:
+
+    int playerOneGrid(int l, int w);
+
+    int playertwoGrid(int l,int w);
+
+//getters and setters
+
+  int get_length();
+
+  void set_length(int l);
+
+  int get_width();
+
+  int set_width(int w);
+
+
+
+
+
+
+
+};
+#endif
+
