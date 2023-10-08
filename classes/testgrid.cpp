f7af2b959cc87ac4e7a5edfa94df5be0340d9677
Shebboliciouss <140377448+Shebboliciouss@users.noreply.github.com>
2023-10-08 16:51:36 +0800

    Update testgrid.cpp

--- classes/testgrid.cpp
+++ classes/testgrid.cpp
@@ -1,22 +1,26 @@
-#include "grid.cpp"
+#include "grid.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 int main(){
 
     bool roundEnd= 0;
 
-    grid gameStart;
+    grid poop(10,10);
 
-    gameStart.playerOneGrid(11,11);
+     std::vector<std::vector<int>> playerTwo= poop.playerTwoGrid();
 
-    return 0;
-
-
+    for (int i=0;i< poop.get_length();i++){
 
 
+        for (int j=0;j<poop.get_width();j++){
+
+            std::cout<<playerTwo[i][j]<<" ";
 
 
+        }
 
-}
+        std::cout<<std::endl;
+    }
