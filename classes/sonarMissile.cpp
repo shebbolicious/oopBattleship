4312b51c9788fab122c2896163d055e5f17856a5
a1889299 <140368334+a1889299@users.noreply.github.com>
2023-10-11 11:09:11 +1030

    Create sonarMissile.cpp

---
+++ classes/sonarMissile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include "missile.h"
+#include "sonarMissile.h"
+#include "grid.h"
+
+void sonarMissile::revealShips() {
+    //code to reveal an area of the grid to show ships for a small time
+}
+
