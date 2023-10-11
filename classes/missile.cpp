6db6cf9df945a2c4c146a72eab188b2971993837
a1889299 <140368334+a1889299@users.noreply.github.com>
2023-10-11 11:10:50 +1030

    Update missile.cpp

--- classes/missile.cpp
+++ classes/missile.cpp
@@ -6,12 +6,13 @@ using namespace std;
 
 bool missile::isFired() {
     return 0;
+    //code to check if the missile has been fired on players turn
 }
 
 bool missile::isHit() {
-    return 0;
+    //code to check if the missile hit a ship
 }
 
-void missile::place() {
-
+void missile::fire() {
+    //code to fire missile
 }
