ce61b726792e84a3470fdb4c706db53186d0087e
Shebboliciouss <140377448+Shebboliciouss@users.noreply.github.com>
2023-10-11 13:39:52 +0800

    Update ship.h

--- classes/ship.h
+++ classes/ship.h
@@ -6,6 +6,9 @@
 
 #include<string>
 
+#include "grid.h"
+
+//double grid for ech player; use switch statement for the alphabetical parts;use if statemetn to check for availabiltiy of ships in that spcae
 class ship {
 
     private:
@@ -16,13 +19,52 @@ class ship {
 
     int width;
 
+    int xcoords;
+
+    int ycoords;
+
+    int numHits;
+
+    bool isValid;
 
 
     public:
 
+    ship();
+
+    ship(std::string name,int widths, int lengths, int xcoords,int ycoords);
+
     int numberOfhits();
 
-    virtual void placeShip();
+    virtual void placeShip( grid& shipGrid);
+
+    
+
+    //getters and setters
+
+
+
+   int get_length();
+
+   int get_width();
+
+   int get_ycoords();
+
+   int get_xcoords();
+
+   void set_NumHits(int hitnum);
+
+   void set_length(int lengths);
+
+   void set_width(int widths );
+
+   void set_ycoords(int y);
+
+   void set_xcoords(int x);
+
+
+
+
 
 
 };
