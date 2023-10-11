18de316972b4774e6bdf0f628b41311bbabac3c5
a1889299 <140368334+a1889299@users.noreply.github.com>
2023-10-11 10:49:56 +1030

    Create missile.cpp

---
+++ classes/missile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "missile.h"
+
+using namespace std;
+
+bool missile::isFired() {
+    return 0;
+}
+
+bool missile::isHit() {
+    return 0;
+}
+
+void missile::place() {
+
+}
