e86ced183714efb45a38439fb5a1d5ccf1bcd58a
Shebboliciouss <140377448+Shebboliciouss@users.noreply.github.com>
2023-10-16 01:45:16 +0800

    Update main.cpp works now for player one ships

--- main.cpp
+++ main.cpp
@@ -1,207 +1,306 @@
+#include <iostream>
+#include <limits>
+#include <string>
+
 #include "Grid.h"
-#include "StraightShip.h"
 #include "LShapedShip.h"
 #include "Missile.h"
-#include <iostream>
-
+#include "StraightShip.h"
 using namespace std;
 
-    int main() {
-        // Initialize game objects
-        Grid player1Grid;
-        Grid player2Grid;
+int main() {
+  // Initialize game objects
+  Grid player1Grid;
+  Grid player2Grid;
 
-        bool validInput = false;
-        int ship1[2];
-        bool isHorizontal = true;
-        string isHorizontalInput;
+  bool validInput = false;
+  int ship1[2];
+  bool isHorizontal = true;     
+  string isHorizontalInput;
 
-        bool playerOneship = true;
+  bool playerOneship = true;
 
-        bool playerTwoship =true;
-        int playerOnecount =0;
+  bool playerTwoship = true;
+  int playerOnecount = 0;
 
-        int playerTwocount=0;
+  int playerTwocount = 0;
 
-        std::vector<StraightShip> ships;
+  std::vector<StraightShip> ships;
 
-        std::vector<StraightShip> ships2;
+  std::vector<StraightShip> ships2;
 
+  // initialise a loop that asks player 1 and two to set up ships
 
-        //initialise a loop that asks player 1 and two to set up ships
+  while (playerOneship) {
+    cout << "player ones turn! \n" << endl;
+    player1Grid.display();
 
-        while(playerOneship){
+    while (playerOnecount < 4) {
+      // char
 
-            cout<<"player ones turn!"<<endl;
+      cout << "\n Is this ship vertical or horizontal (v or h): " << endl;
 
-            while( playerOnecount<4){
-
-               // char
-
-                
-
-                
-    cout << "Is this ship vertical or horizontal (v or h): " << endl;
-
-    while (true) {
+      while (true) {
         cin >> isHorizontalInput;
-        
+
         if (isHorizontalInput == "v" || isHorizontalInput == "h") {
-            if (isHorizontalInput == "h") {
-                isHorizontal = false;
-            }
-            break; // Exit the loop when valid input is provided
+          if (isHorizontalInput == "h") {
+            isHorizontal = false;
+          }
+          break;  // Exit the loop when valid input is provided
         } else {
-            cout << "Invalid input. Enter 'v' for vertical or 'h' for horizontal: " << endl;
+          cout
+              << "Invalid input. Enter 'v' for vertical or 'h' for horizontal: "
+              << endl;
         }
-    }
+      }
 
-        cout << "Enter ship "<< playerOnecount +1<< " coordinates (y,x) ": " << endl;
-        while (!validInput) {
+      cout << "Enter ship " << playerOnecount + 1 << " coordinates (y,x) "
+           << endl;
+      while (!validInput) {
         cin >> ship1[0] >> ship1[1];
-        if (player1Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) == false) {
-            cout << "Ship is out of bounds or overlaps. Please reEnter ship  coordinates:" << endl;
-        } else if (player1Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) == true) {
-                validInput = true;
-                break;
+        if (player1Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) ==
+            false) {
+          cout << "Ship is out of bounds or overlaps. Please reEnter ship  "
+                  "coordinates:"
+               << endl;
+        } else if (player1Grid.isValidPlacement(ship1[0], ship1[1], 2,
+                                                isHorizontal) == true) {
+          validInput = true;
+          break;
         }
-        };
+      };
 
+      StraightShip Ship(ship1[0], ship1[1], 2, isHorizontal);
+      ships.push_back(Ship);
 
-            StraightShip Ship(ship1[0], ship1[1], 2, isHorizontal);
-            ships.push_back(Ship);
+      player1Grid.placeShip(ships[playerOnecount]);
 
-        player1Grid.placeShip(ships[playerOnecount]);
+      player1Grid.display();
 
-        player1Grid.display();
+      playerOnecount = playerOnecount + 1;
+      isHorizontal = true;
+      validInput = false;
+    };
 
+    playerOneship = false;
+  };
 
-        playerOnecount= playerOnecount+1;
-        isHorizontal = true;
-        validInput = false;
+  // Create ships for player 2
 
+    while (playerTwoship) {
+    player2Grid.display();
 
+    cout << "player twos turn! \n" << endl;
 
-        };
+    while (playerTwocount < 4) {
+      // char
 
-        playerOneship=false;
+      cout << "/n Is this ship  vertical or horizontal (v or h): " << endl;
 
-        };
-
-    // Create ships for player 2
-
-    
-        while(playerTwoship){
-
-            cout<<"player twos turn!"<<endl;
-
-            while( playerTwocount<4){
-
-               // char
-
-                
-
-
-
-        cout << "Is this ship  vertical or horizontal (v or h): " << endl;
-
-    while (true) {
+      while (true) {
         cin >> isHorizontalInput;
-        
+
         if (isHorizontalInput == "v" || isHorizontalInput == "h") {
-            if (isHorizontalInput == "h") {
-                isHorizontal = false;
-            }
-            break; // Exit the loop when valid input is provided
+          if (isHorizontalInput == "h") {
+            isHorizontal = false;
+          }
+          break;  // Exit the loop when valid input is provided
         } else {
-            cout << "Invalid input. Enter 'v' for vertical or 'h' for horizontal: " << endl;
+          cout
+              << "Invalid input. Enter 'v' for vertical or 'h' for horizontal: "
+              << endl;
         }
-    }
+      }
 
-        cout << "Enter ship "<<playerTwocount+1<< " coordinates (y,x) " << endl;
-        while (!validInput) {
+      cout << "Enter ship " << playerTwocount + 1 << " coordinates (y,x) "
+           << endl;
+      while (!validInput) {
         cin >> ship1[0] >> ship1[1];
-        if (player2Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) == false) {
-            cout << "Ship is out of bounds or overlaps. please reenter ships coordinates:" << endl;
-        } else if (player2Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) == true) {
-                validInput = true;
-                break;
+        if (player2Grid.isValidPlacement(ship1[0], ship1[1], 2, isHorizontal) ==
+            false) {
+          cout << "Ship is out of bounds or overlaps. please reenter ships "
+                  "coordinates:"
+               << endl;
+        } else if (player2Grid.isValidPlacement(ship1[0], ship1[1], 2,
+                                                isHorizontal) == true) {
+          validInput = true;
+          break;
         }
-        };
+      };
 
+      StraightShip Ship(ship1[0], ship1[1], 2, isHorizontal);
+      ships2.push_back(Ship);
 
-            StraightShip Ship(ship1[0], ship1[1], 2, isHorizontal);
-            ships2.push_back(Ship);
+      player2Grid.placeShip(ships2[playerOnecount]);
 
-        player2Grid.placeShip(ships2[playerOnecount]);
+      player2Grid.display();
 
-        player2Grid.display();
+      playerTwocount = playerTwocount + 1;
+      isHorizontal = true;
+      validInput = false;
+    };
 
+    playerTwoship = false;
+  };
 
-        playerTwocount= playerTwocount+1;
-        isHorizontal = true;
-        validInput = false;
+  
 
+  int shipcount1= 0;
 
+  int shipcount2 =0;
 
-        };
+  bool gameOver = false;
 
-        playerTwoship=false;
+  Grid playerOneShadow;
 
-        };
+  Grid playerTwoShadow;
 
-   
+  bool playerOneTurn=true;
 
+  bool playerTwoTurn = true;
 
-    
+  while (!gameOver){
 
-    while (!player1Grid.isGameOver() && !player2Grid.isGameOver()) {
-        // Player 1's turn
-        int player1X, player1Y;
-        std::cout << "Player 1's turn! Enter coordinates (x, y): ";
-        std::cin >> player1X >> player1Y;
+    while (playerOneTurn){
 
-        if (player2Grid.checkHit(player1X, player1Y)) {
-            std::cout << "Player 1 hit Player 2's ship!\n";
-        } else {
-            std::cout << "Player 1 missed!\n";
-        }
+        playerOneShadow.display();
 
-        // Display grids
-        std::cout << "Player 1's Grid:\n";
-        player1Grid.display();
-        std::cout << "Player 2's Grid:\n";
-        player2Grid.display();
+        if(shipcount1 == 8){
 
-        // Check if Player 1 has won
-        if (player2Grid.isGameOver()) {
-            std::cout << "Player 1 wins!\n";
+            std::cout<<"Player one wins !"<<endl;
+
+            gameOver= true;
+
             break;
+
         }
 
-        // Player 2's turn
-        int player2X, player2Y;
-        std::cout << "Player 2's turn! Enter coordinates (x, y): ";
-        std::cin >> player2X >> player2Y;
+        int Missle1x,Missle1y;
 
-        if (player1Grid.checkHit(player2X, player2Y)) {
-            std::cout << "Player 2 hit Player 1's ship!\n";
-        } else {
-            std::cout << "Player 2 missed!\n";
+        cout<< "Enter Missile coordinates (x,y)";
+
+        cin>> Missle1x>>Missle1y;
+
+        Missile missile(playerOneShadow,Missle1x,Missle1y);
+
+        while ( !missile.isvalidShot(Missle1x,Missle1y)){
+
+            std::cout<<"invalide input, please reenter coordinates"<<endl;
+
+            cin>> Missle1x>> Missle1y;
+
+        
         }
 
-        // Display grids
-        std::cout << "Player 1's Grid:\n";
-        player1Grid.display();
-        std::cout << "Player 2's Grid:\n";
-        player2Grid.display();
+        if(player1Grid.Getcell(Missle1x,Missle1y)=='|' || player1Grid.Getcell(Missle1x,Missle1y)=='-' ){
 
-        // Check if Player 2 has won
-        if (player1Grid.isGameOver()) {
-            std::cout << "Player 2 wins!\n";
-            break;
+            playerOneShadow.Setcell(Missle1x,Missle1y,'O');
+
+            shipcount1=shipcount1+1;
+
+        }else{
+
+            playerOneShadow.Setcell(Missle1x,Missle1y,'X');
+;
+
+
+
         }
-    }
 
-    return 0;
+
+
+
+
+
+        }
+  }
+
 }
+
+
+
+
+
+
+        
+
+
+
+
+
+    
+    
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  
+
+  //while (!player1Grid.isGameOver() && !player2Grid.isGameOver()) {
+    // Player 1's turn
+   // int player1X, player1Y;
+    //std::cout << "Player 1's turn! Enter coordinates (x, y): ";
+   // std::cin >> player1X >> player1Y;
+
+   // if (player2Grid.checkHit(player1X, player1Y)) {
+      //std::cout << "Player 1 hit Player 2's ship!\n";
+   // } else {
+     // std::cout << "Player 1 missed!\n";
+   // }
+
+    // Display grids
+   // std::cout << "Player 1's Grid:\n";
+   // player1Grid.display();
+   // std::cout << "Player 2's Grid:\n";
+   // player2Grid.display();
+
+    // Check if Player 1 has won
+    //if (player2Grid.isGameOver()) {
+     // std::cout << "Player 1 wins!\n";
+     // break;
+    //}
+
+    // Player 2's turn
+    //int player2X, player2Y;
+    //std::cout << "Player 2's turn! Enter coordinates (x, y): ";
+   // std::cin >> player2X >> player2Y;
+
+    //if (player1Grid.checkHit(player2X, player2Y)) {
+   //   std::cout << "Player 2 hit Player 1's ship!\n";
+   // } else {
+    //  std::cout << "Player 2 missed!\n";
+    //}
+
+    // Display grids
+    //std::cout << "Player 1's Grid:\n";
+   // player1Grid.display();
+   // std::cout << "Player 2's Grid:\n";
+  //  player2Grid.display();
+
+    // Check if Player 2 has won
+   // if (player1Grid.isGameOver()) {
+   //   std::cout << "Player 2 wins!\n";
+   //   break;
+  //  }
+ // }
+
+ // return 0;
